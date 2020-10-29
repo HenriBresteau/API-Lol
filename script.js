542fbc028e09273532cd9dd94fe3c52155da030a
henriB <henri.bretseau@gmail.com>
2020-10-29 11:13:20 +0100

    methode fetch

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
-function generateJoke() {
-    // API
-    const request = new XMLHttpRequest();
+// function generateJoke() {
+//     // API
+//     const request = new XMLHttpRequest();
 
-    request.open('GET', 'https://api.icndb.com/jokes', true)
-    request.onload = function () {
-        const data = JSON.parse(this.response);
-        console.log(data.value);
+//     request.open('GET', 'https://api.icndb.com/jokes', true)
+//     request.onload = function () {
+//         const data = JSON.parse(this.response);
+//         console.log(data.value);
+//         const length= Object.keys(data.value).length;
+//         console.log(length);
+
+//         const item = data.value[Math.floor(Math.random() * length)];
+//         console.log(item);
+//         document.getElementById('joke').innerHTML = `${item.joke}`;
+//     }
+//     request.send();
+// };
+
+const generateJoke= () => {
+    fetch('https://api.icndb.com/jokes')
+    .then(res => res.json())
+    .then(data => {
         const length= Object.keys(data.value).length;
-        console.log(length);
-
         const item = data.value[Math.floor(Math.random() * length)];
-        console.log(item);
         document.getElementById('joke').innerHTML = `${item.joke}`;
-    }
-    request.send();
-};
-
+    })
+}
+generateJoke();
